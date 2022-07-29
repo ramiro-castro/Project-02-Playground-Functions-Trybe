3d85d5a9489fed13ce274c45877144da6e80b698
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-07-29 16:22:23 -0300

    generatePhoneNumber

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,46 @@
 // Desafio 11
 function generatePhoneNumber(contato) {
   // seu código aqui
+  let aparece =0;
+  let obsIndex=0;
+  let formata = [];
+  if(contato.length!=11){
+	return 'Array com tamanho incorreto.'
+  }
 
+
+  for(let index=0; index<contato.length;index+=1){
+	if(contato[index]<0){
+		return 'não é possível gerar um número de telefone com esses valores';
+	}else if(contato[index]>9){
+		return 'não é possível gerar um número de telefone com esses valores';
+	}
+  }
+
+  for(let index=0; index<contato.length;index+=1){
+	//console.log("aqui index");
+	for(let index2 = 0; index2 < contato.length; index2+=1){
+		//console.log("aqui y");
+		if(contato[index]==contato[index2]){
+			
+			aparece+=1;
+			
+			//console.log(contato[index] + " aparece " + aparece +" vez(es).");
+			if (aparece>=3){
+				return 'não é possível gerar um número de telefone com esses valores';
+			}
+		}
+		
+	}
+	aparece = 0;
+
+  }
+  formata=['(',contato[0],contato[1],') ',contato[2],contato[3],contato[4],contato[5],contato[6],'-',contato[7],contato[8],contato[9],contato[10]];
+  return formata.join('')
 
 }
+
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(lineA,lineB,lineC) {
