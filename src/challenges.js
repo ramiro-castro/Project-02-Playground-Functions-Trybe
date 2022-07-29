a2f6420526ad486678a7acf2d7697cb861d462cf
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-07-29 15:29:30 -0300

    triangleCheck

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,10 @@
 // Desafio 1
-function compareTrue(a, b) {
-  if(a == true && b==true){
-	return true;
-  }else{
-	return false;
-  }
-}
-
-const girafa = true;
-const elefante = true;
-const macaco = false;
-
-//console.log(compareTrue(macaco, macaco));
+ function compareTrue(a, b) {
+ if (a === true && b === true) {
+ return true;
+ }
+ return false;
+ }
 
 
 // Desafio 2
@@ -19,9 +12,6 @@ function calcArea(base, altura) {
   // seu código aqui
   return ((base * altura) / 2);
 }
-
-//console.log(calcArea(51,1));
-
 // Desafio 3
 function splitSentence(frase) {
 
@@ -42,36 +32,19 @@ let impedeExcesso=0;
 
 	for(let index=0; index<frase.length; index+=1){
 		if(frase[index] == ' '){
-			
-			//if(allEspacos[contEspacos]!=index-1){
+		
 				allEspacos[contEspacos] = index;
 				contEspacos+=1;
-			//}
-			//console.log(allEspacos[0]+"="+index);
-			//else if(allEspacos[contEspacos]==index-1){
-			// 	impedeExcesso=1;
-			// }else{
-			// 	impedeExcesso=0;
-			// }
+
 		}	
 	}
 
-	// for(x=0;x<allEspacos.length;x+=1){
-
-	// }
-	// console.log("espacos "+allEspacos);
-
 	for(let index=0; index<frase.length; index+=1){
-		//console.log("teste for inicial");
 		if(frase[index] == ' ' || index==frase.length-1){
-			//console.log("fora do if "+index);
 			if(frase[index] == ' '){
-				//caso index chegue ao final do vetor, esse if impede de pegar apenas a ultima letra da palavra final.
-				// if(maisEspaco=0){
-				// 	maisEspaco=1;
-				console.log("fora do if posiciona "+index);
+			
 				if(flag!=0 && espaco!=allEspacos.length-1){
-					console.log("dentro if posiciona "+index);
+					
 					posicionador = allEspacos[espaco]+1;//posicao seguite ao espaco existente
 					espaco+=1; 
 					posicionador2 = allEspacos[espaco];//nao precisa do +1 pois no for tem menor, e nao menor igual
@@ -80,41 +53,31 @@ let impedeExcesso=0;
 					posicionador=index;
 				}
 				
-				// }else{
-				// 	maisEspaco=0;
-				// 	posicionador2=
-
-				// }
-
-				console.log(index);
 			}else if(contEspacos==0){
 				for(let y=0; y<frase.length; y+=1){
 					
 					array[cont]=frase[y];
-					//console.log("index2 "+frase[i]);
 					cont+=1;
 				}
 				string = array.join('');
 				array2[cont2]=string;
 				return array2; // para frases com uma unica palavra
+		
 			}else if(index==frase.length-1){
-				console.log("dentro do else if posiciona "+index);
 				posicionador = allEspacos[espaco]+1;//ultimo espaco existente
 				posicionador2=index+1;
 			}
-			//console.log("teste if inicial ");
+			
 			if(flag == 0){
 				flag=1;
 				
 				for(let index2=0; index2<posicionador; index2+=1){
 					array[cont]=frase[index2];
-					console.log("index2 "+frase[index2]);
 					cont+=1;
 				}
 				string = array.join('');
 				array2[cont2]=string;
 
-				console.log("If Flag 0 " +array2);
 				cont2+=1;
 				array = [];//zera para evitar o lixo de outras palavras
 				cont = 0;
@@ -125,31 +88,23 @@ let impedeExcesso=0;
 				for(let index3=posicionador; index3<posicionador2; index3+=1){
 					if(frase[index] != ''){
 					array[cont]=frase[index3];
-					console.log("index3 aqui "+index3+frase[index3]);
+					
 					cont+=1;
 					}
 				}
 				string = array.join('');
 				array2[cont2]=string;
-				console.log("If Flag 1 " +array2);
 				cont2+=1;
 				array = [];//zera para evitar o lixo de outras palavras
 				cont = 0;
 
 			}
 
-		}else{
-
 		}
-		
-
 	}
-
 	return array2;
 }
-
-	//console.log(splitSentence("ramiro de castro cavalcanti"));
-
+/*
 // Desafio 4
 function concatName(array) {
   // seu código aqui
@@ -167,22 +122,16 @@ function concatName(array) {
 
 	return string;
 }
-
-//console.log(concatName(['captain', 'my', 'captain']));
-
 // Desafio 5
 function footballPoints(wins,ties) {
   // seu código aqui
   return wins*3+ties*1;
 }
-//console.log(footballPoints(5,8));
-
 // Desafio 6
 function highestCount(array) {
   // seu código aqui
   let maior=-1;
   let contMaior=0;
-	
 	
 		for(let x = 0; x < array.length; x+=1){
 			
@@ -193,26 +142,18 @@ function highestCount(array) {
 						maior=array[x];
 					
 				}
-				
-				// console.log(array[x]+"="+array[y]);
-				// console.log(maior);
-				
 			}
 		
 		}
 	for(let x = 0; x < array.length; x+=1){
 		if(maior==array[x]){
-			//console.log("if maior "+array[x]+"="+maior);
 			contMaior+=1;	
-			//console.log(contMaior);	
 		}
 	}
 return contMaior;	
 
 	return maior;
 }
-console.log(highestCount([-2, -2, -1]));
-
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
@@ -227,26 +168,14 @@ function catAndMouse(mouse,cat1,cat2) {
 
 		return d;
 	}
-	//console.log("d m e c1 "+distancia(mouse,cat1));
-	//console.log("d m e c2 "+distancia(mouse,cat2));
-
 	if(distancia(mouse,cat1)>distancia(mouse,cat2)){
-		
 		return 'cat2';
-
 	}else if(distancia(mouse,cat1)<distancia(mouse,cat2)){
-		
 		return 'cat1';
-
 	}else{
 		return 'os gatos trombam e o rato foge';
 	}
-
-
 }
-
-//console.log(catAndMouse(1, 0, 2));
-
 // Desafio 8
 function fizzBuzz(array) {
   // seu código aqui
@@ -270,8 +199,6 @@ function fizzBuzz(array) {
 	return array;
 
 }
-//console.log(fizzBuzz([2, 15, 7, 9, 45]));
-
 // Desafio 9
 function encode(frase) {
   // seu código aqui
@@ -294,14 +221,11 @@ function encode(frase) {
 		}else if(encode[index]=='u'){
 			encode[index]='5';
 		}
-		//console.log(index + ' e ' + encode[index]);
 	}
 	frase = encode.join('');
 	return frase;
 
 }
-
-//console.log(encode("How are you today?"));
 function decode(frase) {
 	// seu código aqui
 	let decode = [];
@@ -323,14 +247,10 @@ function decode(frase) {
 		}else if(decode[index]=='5'){
 			decode[index]='u';
 		}
-		//console.log(index + ' e ' + decode[index]);
 	}
 	frase = decode.join('');
 	return frase;
 }
-
-//console.log(decode("h3 th2r2!"));
-
 // Desafio 10
 function techList(array, pessoa) {
   // seu código aqui
@@ -355,8 +275,6 @@ function techList(array, pessoa) {
 	return vetor;
 
 }
-//console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],'Lucas'));
-
 module.exports = {
   calcArea,
   catAndMouse,
@@ -370,3 +288,4 @@ module.exports = {
   splitSentence,
   techList,
 };
+*/
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,28 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(contato) {
   // seu código aqui
+
+
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA,lineB,lineC) {
   // seu código aqui
-}
+  let if1;
+  let if2;
+  if (lineA+lineB>lineC && lineB+lineC>lineA && lineC+lineA>lineB){
+    if1 = 1;
+  }
+  if (Math.abs(lineA-lineB)<lineC && Math.abs(lineB-lineC)<lineA && Math.abs(lineC-lineA)<lineB){
+	if2 = 1;
+  }
 
+  if(if1 == 1 && if2 == 1){
+		return true;
+
+  }
+  return false;
+}
 // Desafio 13
 function hydrate() {
   // seu código aqui
